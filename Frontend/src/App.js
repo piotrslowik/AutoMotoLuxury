216349cf5c8bb1ec9aa716eda87653a3bd338ae0
piotrslowik <piotr.j.slowik@gmail.com>
2021-11-09 14:36:55 +0100

    Added Rebass.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -10,20 +10,25 @@ import Error404 from './pages/Error404';
 import { Provider } from 'react-redux';
 import store from './store';
 
+import { ThemeProvider } from '@emotion/react'
+import theme from '@rebass/preset'
+
 import './stylesheets/App.scss';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={MainPage} />
-          <Route exact path="/offer" component={NewOffer} />
-          <Route exact path="/offer/:offerId" component={Offer} />
-          <Route exact path="/404" component={Error404} />
-          <Redirect to="/404" />
-        </Switch>
-      </Router>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={MainPage} />
+            <Route exact path="/offer" component={NewOffer} />
+            <Route exact path="/offer/:offerId" component={Offer} />
+            <Route exact path="/404" component={Error404} />
+            <Redirect to="/404" />
+          </Switch>
+        </Router>
+      </ThemeProvider>
     </Provider>
   );
 }
