9e0dba40dfe8ae3c7b1ec575ab7b8a2603328beb
piotrslowik <piotr.j.slowik@gmail.com>
2021-11-09 13:50:44 +0100

    App.js cleanup

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,4 +1,3 @@
-// import React, { useEffect, useState } from 'react'
 import React from 'react';
 
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
@@ -8,45 +7,12 @@ import NewOffer from './pages/NewOffer';
 import Offer from './pages/Offer';
 import Error404 from './pages/Error404';
 
-// import DataContext from './context/data-context';
-
 import { Provider } from 'react-redux';
 import store from './store';
 
 import './stylesheets/App.scss';
-// import { LocalStorageGet, LocalStorageSave } from './logic/helpers';
 
 const App = () => {
-  // const [fuels, setFuels] = useState([]);
-  // const [origins, setOrigins] = useState([]);
-  // const [makes, setMakes] = useState([]);
-
-  // useEffect(() => {
-  //   (async () => {
-  //     try {
-  //       handleDataInit('fuels', getFuels, setFuels);
-  //       handleDataInit('origins', getOrigins, setOrigins);
-  //       handleDataInit('makes', getMakes, setMakes);
-  //     }
-  //     catch (error) {
-  //       throw error;
-  //     }
-  //   })();
-  // }, []);
-
-  // const handleDataInit = async (array, getter, setter) => {
-  //   const storage = LocalStorageGet(array);
-  //   if (storage) {
-  //     const result = JSON.parse(storage);
-  //     setter(result);
-  //   }
-  //   else {
-  //     const result = await getter();
-  //     LocalStorageSave(array, JSON.stringify(result));
-  //     setter(result);
-  //   }
-  // }
-
   return (
     <Provider store={store}>
       <Router>
