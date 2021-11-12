d7568dbf69f7b94845aee06a6542a70af266c9d2
piotrslowik <piotr.j.slowik@gmail.com>
2021-11-12 13:40:18 +0100

    Filter component optimized

--- Frontend/src/components/Partials/Filters/index.js
+++ Frontend/src/components/Partials/Filters/index.js
@@ -13,237 +13,244 @@ import { getModels } from '../../../logic/graphql/model';
 import SingleSelect from '../../Shared/Select';
 
 const Filters = () => {
-    const dummyObj = {
-        text: 'Wszystkie',
-        id: '0',
-    }
-
-    const { fuels, origins, makes } = useSelector(state => state.parameters);
-    const [models, setModels] = useState([]);
-
-    const [fuel, setFuel] = useState(dummyObj.id);
-    const [origin, setOrigin] = useState(dummyObj.id);
-    const [make, setMake] = useState(dummyObj.id);
-    const [model, setModel] = useState(dummyObj.id);
-    const [loading, setLoading] = useState(true);
-    const [kmsMax, setKmsMax] = useState('');
-    const [kmsMin, setKmsMin] = useState('');
-    const [priceMin, setPriceMin] = useState('');
-    const [priceMax, setPriceMax] = useState('');
-    const [yearMin, setYearMin] = useState('');
-    const [yearMax, setYearMax] = useState('');
-
-    const dispatch = useDispatch();
-  
-    useEffect(() => {
-      fetchParameters();
-    }, [dispatch]);
-
-    useEffect(() => {
-        if (make !== dummyObj.id) {
-            fetchModels(make);
-        } else {
-            setModels([]);
-        }
-    }, [make]);
-  
-    const fetchParameters = async () => {
-      const fuels = await getFuels();
-      setFuels(fuels);
-      const origins = await getOrigins();
-      setOrigins(origins);
-      const makes = await getMakes();
-      setMakes(makes);
-      setLoading(false);
+  const dummyObj = {
+    text: 'Wszystkie',
+    id: '0',
   }
-  
-    const setFuels = param => {
-        dispatch(parametersActions.setFuels(param.map(el => el)));
-    }
-    const setOrigins = param => {
-      dispatch(parametersActions.setOrigins(param.map(el => el)));
-    }
-    const setMakes = param => {
-      dispatch(parametersActions.setMakes(param.map(el => el)));
-    }
 
-    const fetchModels = async make =>{
-        const models = await getModels(make);
-        setModels(models);
+  const { fuels, origins, makes } = useSelector(state => state.parameters);
+  const [models, setModels] = useState([]);
+
+  const [fuel, setFuel] = useState(dummyObj.id);
+  const [origin, setOrigin] = useState(dummyObj.id);
+  const [make, setMake] = useState(dummyObj.id);
+  const [model, setModel] = useState(dummyObj.id);
+  const [loading, setLoading] = useState(true);
+  const [kmsMax, setKmsMax] = useState('');
+  const [kmsMin, setKmsMin] = useState('');
+  const [priceMin, setPriceMin] = useState('');
+  const [priceMax, setPriceMax] = useState('');
+  const [yearMin, setYearMin] = useState('');
+  const [yearMax, setYearMax] = useState('');
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    fetchParameters();
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (make !== dummyObj.id) {
+      fetchModels(make);
+    } else {
+      setModels([]);
     }
+  }, [make]);
+  useEffect(() => {
+    setMake(dummyObj.id);
+    setModel(dummyObj.id);
+  }, [origin]);
 
-    const formatArray = (arr, field) => {
-        if (!arr) return [];
-        const result = arr.map(el => { return {text: el[field], id: el._id}});
-        result.unshift(dummyObj);
-        return result;
+  const fetchParameters = async () => {
+    const fuelsPromise = getFuels();
+    const originsPromise = getOrigins();
+    const makesPromise = getMakes();
+    Promise.all([fuelsPromise, originsPromise, makesPromise])
+      .then(values => {
+        setFuels(values[0]);
+        setOrigins(values[1])
+        setMakes(values[2]);
+        setLoading(false);
+      });
+  }
+
+  const setFuels = param => {
+    dispatch(parametersActions.setFuels(param.map(el => el)));
+  }
+  const setOrigins = param => {
+    dispatch(parametersActions.setOrigins(param.map(el => el)));
+  }
+  const setMakes = param => {
+    dispatch(parametersActions.setMakes(param.map(el => el)));
+  }
+
+  const fetchModels = async make =>{
+    const models = await getModels(make);
+    setModels(models);
+  }
+
+  const formatArray = (arr, field) => {
+    if (!arr) return [];
+    const result = arr.map(el => { return {text: el[field], id: el._id}});
+    result.unshift(dummyObj);
+    return result;
+  }
+
+  const formatMakeArray = (arr, field) => {
+    if (origin === 0) {
+      return formatArray(arr, field);
+    } else {
+      const filtered = arr.filter(make => make.origin._id === origin);
+      return formatArray(filtered, field);
     }
+  }
 
-    const formatMakeArray = (arr, field) => {
-        if (origin === 0) {
-            return formatArray(arr, field);
-        } else {
-            const filtered = arr.filter(make => make.origin._id === origin);
-            return formatArray(filtered, field);
-        }
-    }
-  
-    const currentYear = new Date().getFullYear();
+  const currentYear = new Date().getFullYear();
 
-    return (
-        <Card>
-            {loading ? null :(
-            <CardContent>
-                <Grid container spacing={3}>
-                    <Grid item xs={12} md={6}>
-                        <Grid item xs={12}>
-                            <Typography fontWeight='700' ml={2}>
-                                Paliwo
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <SingleSelect
-                                items={formatArray(fuels, 'fuel')}
-                                value={fuel}
-                                onChange={setFuel}
-                            />
-                        </Grid>
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                        <Grid item xs={12}>
-                            <Typography fontWeight='700' ml={2}>
-                                Pochodzenie
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <SingleSelect
-                                items={formatArray(origins, 'origin')}
-                                value={origin}
-                                onChange={setOrigin}
-                            />
-                        </Grid>
-                    </Grid>
+  return (
+    <Card>
+      {loading ? null :(
+      <CardContent>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={6}>
+            <Grid item xs={12}>
+              <Typography fontWeight='700' ml={2}>
+                Paliwo
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <SingleSelect
+                items={formatArray(fuels, 'fuel')}
+                value={fuel}
+                onChange={setFuel}
+              />
+            </Grid>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Grid item xs={12}>
+              <Typography fontWeight='700' ml={2}>
+                  Pochodzenie
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <SingleSelect
+                  items={formatArray(origins, 'origin')}
+                  value={origin}
+                  onChange={setOrigin}
+              />
+            </Grid>
+          </Grid>
 
-                    <Grid item xs={12} md={6}>
-                        <Grid item xs={12}>
-                            <Typography fontWeight='700' ml={2}>
-                                Marka
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <SingleSelect
-                                items={formatMakeArray(makes, 'make')}
-                                value={make}
-                                onChange={setMake}
-                            />
-                        </Grid>
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                        <Grid item xs={12}>
-                            <Typography fontWeight='700' ml={2}>
-                                Model
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <SingleSelect
-                                items={formatArray(models, 'model')}
-                                value={model}
-                                onChange={setModel}
-                            />
-                        </Grid>
-                    </Grid>
+          <Grid item xs={12} md={6}>
+            <Grid item xs={12}>
+              <Typography fontWeight='700' ml={2}>
+                  Marka
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <SingleSelect
+                  items={formatMakeArray(makes, 'make')}
+                  value={make}
+                  onChange={setMake}
+              />
+            </Grid>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Grid item xs={12}>
+              <Typography fontWeight='700' ml={2}>
+                  Model
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <SingleSelect
+                  items={formatArray(models, 'model')}
+                  value={model}
+                  onChange={setModel}
+              />
+            </Grid>
+          </Grid>
 
-                    <Grid item xs={12} md={6}>
-                        <Grid item xs={12}>
-                            <Typography fontWeight='700' ml={2}>
-                                Przebieg
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12} sx={{ display: 'flex' }}>
-                            <TextField 
-                                variant="outlined"
-                                type="number"
-                                value={kmsMin}
-                                onChange={e => setKmsMin(e.target.value)}
-                                inputProps={{ inputMode: 'numeric', pattern: '[0-9]*', min: '0', max: '999999', "aria-autocomplete": "off" }}
-                                sx={{ flexGrow: 1 }}
-                            />
-                            <TextField 
-                                variant="outlined"
-                                type="number"
-                                value={kmsMax}
-                                onChange={e => setKmsMax(e.target.value)}
-                                inputProps={{ inputMode: 'numeric', pattern: '[0-9]*', min: '0', max: '999999', "aria-autocomplete": "off" }}
-                                sx={{ ml: 2, flexGrow: 1 }}
-                            />
-                        </Grid>
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                        <Grid item xs={12}>
-                            <Typography fontWeight='700' ml={2}>
-                                Cena
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12} sx={{ display: 'flex' }}>
-                            <TextField 
-                                variant="outlined"
-                                type="number"
-                                value={priceMin}
-                                onChange={e => setPriceMin(e.target.value)}
-                                inputProps={{ inputMode: 'numeric', pattern: '[0-9]*', min: '0', max: '999999', "aria-autocomplete": "off" }}
-                                sx={{ flexGrow: 1 }}
-                            />
-                            <TextField 
-                                variant="outlined"
-                                type="number"
-                                value={priceMax}
-                                onChange={e => setPriceMax(e.target.value)}
-                                inputProps={{ inputMode: 'numeric', pattern: '[0-9]*', min: '0', max: '999999', "aria-autocomplete": "off" }}
-                                sx={{ ml: 2, flexGrow: 1 }}
-                            />
-                        </Grid>
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                        <Grid item xs={12}>
-                            <Typography fontWeight='700' ml={2}>
-                                Rocznik
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12} sx={{ display: 'flex' }}>
-                            <TextField 
-                                variant="outlined"
-                                type="number"
-                                value={yearMin}
-                                onChange={e => setYearMin(e.target.value)}
-                                inputProps={{ inputMode: 'numeric', pattern: '[0-9]*', max: currentYear.toString(), "aria-autocomplete": "off" }}
-                                sx={{ flexGrow: 1 }}
-                            />
-                            <TextField 
-                                variant="outlined"
-                                type="number"
-                                value={yearMax}
-                                onChange={e => setYearMax(e.target.value)}
-                                inputProps={{ inputMode: 'numeric', pattern: '[0-9]*', max: currentYear.toString(), "aria-autocomplete": "off" }}
-                                sx={{ ml: 2, flexGrow: 1 }}
-                            />
-                        </Grid>
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                        <Button
-                            color="secondary"
-                            variant="contained"
-                            size="large"
-                            fullWidth
-                            sx={{ mt: 4 }}
-                        >
-                            Filtruj
-                        </Button>
-                    </Grid>
-                </Grid>
-            </CardContent>
-            )}
-        </Card>
-    )
+          <Grid item xs={12} md={6}>
+            <Grid item xs={12}>
+              <Typography fontWeight='700' ml={2}>
+                Przebieg
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sx={{ display: 'flex' }}>
+              <TextField 
+                variant="outlined"
+                type="number"
+                value={kmsMin}
+                onChange={e => setKmsMin(e.target.value)}
+                inputProps={{ inputMode: 'numeric', pattern: '[0-9]*', min: '0', max: '999999', "aria-autocomplete": "off" }}
+                sx={{ flexGrow: 1 }}
+              />
+              <TextField 
+                variant="outlined"
+                type="number"
+                value={kmsMax}
+                onChange={e => setKmsMax(e.target.value)}
+                inputProps={{ inputMode: 'numeric', pattern: '[0-9]*', min: '0', max: '999999', "aria-autocomplete": "off" }}
+                sx={{ ml: 2, flexGrow: 1 }}
+              />
+            </Grid>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Grid item xs={12}>
+              <Typography fontWeight='700' ml={2}>
+                Cena
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sx={{ display: 'flex' }}>
+              <TextField 
+                variant="outlined"
+                type="number"
+                value={priceMin}
+                onChange={e => setPriceMin(e.target.value)}
+                inputProps={{ inputMode: 'numeric', pattern: '[0-9]*', min: '0', max: '999999', "aria-autocomplete": "off" }}
+                sx={{ flexGrow: 1 }}
+              />
+              <TextField 
+                variant="outlined"
+                type="number"
+                value={priceMax}
+                onChange={e => setPriceMax(e.target.value)}
+                inputProps={{ inputMode: 'numeric', pattern: '[0-9]*', min: '0', max: '999999', "aria-autocomplete": "off" }}
+                sx={{ ml: 2, flexGrow: 1 }}
+              />
+            </Grid>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Grid item xs={12}>
+              <Typography fontWeight='700' ml={2}>
+                Rocznik
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sx={{ display: 'flex' }}>
+              <TextField 
+                variant="outlined"
+                type="number"
+                value={yearMin}
+                onChange={e => setYearMin(e.target.value)}
+                inputProps={{ inputMode: 'numeric', pattern: '[0-9]*', max: currentYear.toString(), "aria-autocomplete": "off" }}
+                sx={{ flexGrow: 1 }}
+              />
+              <TextField 
+                variant="outlined"
+                type="number"
+                value={yearMax}
+                onChange={e => setYearMax(e.target.value)}
+                inputProps={{ inputMode: 'numeric', pattern: '[0-9]*', max: currentYear.toString(), "aria-autocomplete": "off" }}
+                sx={{ ml: 2, flexGrow: 1 }}
+              />
+            </Grid>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Button
+              color="secondary"
+              variant="contained"
+              size="large"
+              fullWidth
+              sx={{ mt: 4 }}
+            >
+              Filtruj
+            </Button>
+          </Grid>
+        </Grid>
+      </CardContent>
+      )}
+    </Card>
+  )
 }
 
 export default Filters;
