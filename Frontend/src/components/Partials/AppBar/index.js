10c0293536926d6f6531e1ba285caba35d69bdaf
piotrslowik <piotr.j.slowik@gmail.com>
2021-11-13 12:01:23 +0100

    responsive behaviour of appbar icons

--- Frontend/src/components/Partials/AppBar/index.js
+++ Frontend/src/components/Partials/AppBar/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useTheme } from '@mui/material';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
-import { Container, Box, AppBar as Bar, Toolbar, Button, Icon } from '@mui/material';
+import { Container, Box, AppBar as Bar, Toolbar, Button, Icon, Fab } from '@mui/material';
 import Logo from '../../Shared/Logo';
 
 const ContactIcon = () => {
@@ -8,8 +10,21 @@ const ContactIcon = () => {
     <Icon>phone</Icon>
   );
 }
+const LoginIcon = () => {
+  return (
+    <Icon>account_circle</Icon>
+  );
+}
+const RegisterIcon = () => {
+  return (
+    <Icon>person_add_alt_1</Icon>
+  );
+}
 
 const AppBar = () => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up('sm'));
+
   return (
     <Box sx={{ flexGrow: 1, mb: 3 }}>
       <Bar position='static' sx={{ backgroundColor: 'primary.dark' }}>
@@ -17,9 +32,23 @@ const AppBar = () => {
           <Toolbar disableGutters>
             <Logo />
             <Box sx={{ flexGrow: 1 }} />
-            <Button variant='outlined' color='white' sx={{ mr: 2 }} startIcon={<ContactIcon />}>Kontakt</Button>
-            <Button variant='contained' sx={{ mr: 2 }}>Logowanie</Button>
-            <Button variant='contained'>Rejestracja</Button>
+            { matches 
+            ? <React.Fragment>
+                <Button variant='outlined' color='white' sx={{ mr: 2 }} startIcon={<ContactIcon />}>Kontakt</Button>
+                <Button variant='contained' sx={{ mr: 2 }}>Logowanie</Button>
+                <Button variant='contained'>Rejestracja</Button>
+              </React.Fragment>
+            : <React.Fragment>
+                <Fab color="white" aria-label="Kontakt" size="small" sx={{ mr: 2 }}>
+                  <ContactIcon />
+                </Fab>
+                <Fab color="white" aria-label="Logowanie" size="small" sx={{ mr: 2 }}>
+                  <LoginIcon />
+                </Fab>
+                <Fab color="white" aria-label="Rejestracja" size="small">
+                  <RegisterIcon />
+                </Fab>
+              </React.Fragment> }
           </Toolbar>
         </Container>
       </Bar>
